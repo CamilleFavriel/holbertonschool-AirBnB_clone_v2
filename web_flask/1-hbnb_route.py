c2420eea14aa0df2f1a947c7dabfda3d0a01ebc1
Meashyshy <camillefvrl@gmail.com>
2023-08-18 05:40:32 -0700

    added multiple routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,5 +10,10 @@ app = Flask(__name__)
 def hello():
     return 'Hello HBNB!'
 
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    return 'HBNB'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,5 +10,15 @@ app = Flask(__name__)
 def hello():
     return 'Hello HBNB!'
 
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_with_text(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
