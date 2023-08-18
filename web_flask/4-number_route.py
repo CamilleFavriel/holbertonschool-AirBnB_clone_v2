e3beb01d3513ca15274e9fb2da21be8bd73bde6f
Meashyshy <camillefvrl@gmail.com>
2023-08-18 06:03:19 -0700

    output error

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -32,9 +32,10 @@ def python_is_cool(text):
     return f"Python {text}"
 
 
-@app.route('/number/<n>', strict_slashes=False)
-def display_int(n):
-    if isinstance(n, int):
+@app.route("/number/<int:n>", strict_slashes=False)
+def display_number(n):
+    """display “n is a number” only if n is an integer"""
+    if type(n) is int:
         return f"{n} is a number"
 
 
